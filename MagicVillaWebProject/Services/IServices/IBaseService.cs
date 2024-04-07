d816d2558fe0b858f589aa41abdcf1781eb1fa8c
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2024-04-07 20:47:55 +0100

    basic api Request and response on the mvc project

---
+++ MagicVillaWebProject/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using MagicVillaWebProject.Models;
+
+namespace MagicVillaWebProject.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
