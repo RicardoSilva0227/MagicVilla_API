994863d1ccc5e7fbae7a11294054556117756919
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2024-04-07 20:22:55 +0100

    Config automapper

---
+++ MagicVillaWebProject/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MagicVillaWebProject.Models.Dto;
+
+namespace MagicVillaWebProject
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            #region Villa
+            CreateMap<VillaDto,VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDto,VillaUpdateDTO>().ReverseMap();
+            #endregion
+
+            #region VillaNumber
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+            #endregion
+        }
+    }
+}
