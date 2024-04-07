286b1e8de9855e27f9f5aec58ee16058a64598d7
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2024-04-07 20:18:02 +0100

    Set up the MVC Project that will consume the API

---
+++ MagicVillaWebProject/Models/APIRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static SD.SD;
+
+namespace MagicVillaWebProject.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
