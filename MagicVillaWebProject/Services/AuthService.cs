aa337c0317bf6cf0dc9cbaeef29562b89d27c794
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2024-04-21 19:37:21 +0100

    added DTO's for user and the auth services on the web project

---
+++ MagicVillaWebProject/Services/AuthService.cs
@@ -0,0 +1,40 @@
+﻿using MagicVillaWebProject.Models;
+using MagicVillaWebProject.Models.Dto;
+using MagicVillaWebProject.Services.IServices;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace MagicVillaWebProject.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+
+        public Task<T> LoginAsync<T>(LoginRequestDto loginRequest)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.SD.ApiType.POST,
+                Data = loginRequest,
+                Url = villaUrl + "/api/UsersAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegistrationRequestDto user)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.SD.ApiType.POST,
+                Data = user,
+                Url = villaUrl + "/api/UsersAuth/register"
+            });
+        }
+    }
+}
---
+++ MagicVillaWebProject/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using MagicVillaWebProject.Models.Dto;
+
+namespace MagicVillaWebProject.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDto loginRequest);
+        Task<T> RegisterAsync<T>(RegistrationRequestDto user);
+    }
+}
