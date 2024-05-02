3b53b110a4a49a58a555a1d3b4dd11d1184d47a1
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2024-05-02 01:28:24 +0100

    Added an auth controller and its base methods, created the views needed for auth (login, register and access denied)

---
+++ MagicVillaWebProject/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using MagicVillaWebProject.Models;
+using MagicVillaWebProject.Models.Dto;
+using MagicVillaWebProject.Services.IServices;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaWebProject.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+                _authService = authService;
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginResponseDto dto)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegistrationRequestDto dto)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(dto);
+            if (result != null && result.IsSuccess)
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout(RegistrationRequestDto dto)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied() 
+        {
+            return View();
+        }
+
+
+    }
+}
